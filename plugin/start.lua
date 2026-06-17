02b2fc36efb96f02f23a13834153c0747200fea9
kurku <sandovin@mail.ru>
2026-06-17 19:09:58 +0300

    chmod(*): remove executable permission from 3 files
    t

--- plugin/start.lua
+++ plugin/start.lua
@@ -1,18 +1,14 @@
-local on_attach = function(client, bufnr)
-  local opts = { noremap = true, silent = true, buffer = bufnr }
-  
-  -- Navigation & Information
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP: Show hover documentation" })
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "LSP: Go to definition" })
-  vim.keymap.set('n', 'gl', vim.lsp.buf.implementation, { buffer = bufnr, desc = "LSP: Go to implementation" })
-  vim.keymap.set('n', '<M-f>', vim.lsp.buf.references, { buffer = bufnr, desc = "LSP: List references" })
+local on_attach = function(_, bufnr)
+  -- Replace standard behaviour (gd, K)
+  vim.keymap.set('n',          'K',     vim.lsp.buf.hover,          { buffer = bufnr, desc = "LSP: Show hover documentation" })
+  vim.keymap.set('n',          'gd',    vim.lsp.buf.definition,     { buffer = bufnr, desc = "LSP: Go to definition"         })
 
-  -- Refactoring & Actions
-  vim.keymap.set('n', '<M-r>', vim.lsp.buf.rename, { buffer = bufnr, desc = "LSP: Rename symbol" })
-  vim.keymap.set({ 'n', 'v' }, '<M-c>', vim.lsp.buf.code_action, { buffer = bufnr, desc = "LSP: Show code actions" })
-
-  -- Diagnostics (Errors, Warnings, etc.)
-  vim.keymap.set('n', '<M-d>', vim.diagnostic.open_float, { buffer = bufnr, desc = "LSP: Show line diagnostics" })
+  -- New mappings introduced
+  vim.keymap.set('n',          'gl',    vim.lsp.buf.implementation, { buffer = bufnr, desc = "LSP: Go to implementation"     })
+  vim.keymap.set('n',          '<M-f>', vim.lsp.buf.references,     { buffer = bufnr, desc = "LSP: List references"          })
+  vim.keymap.set('n',          '<M-r>', vim.lsp.buf.rename,         { buffer = bufnr, desc = "LSP: Rename symbol"            })
+  vim.keymap.set({ 'n', 'v' }, '<M-c>', vim.lsp.buf.code_action,    { buffer = bufnr, desc = "LSP: Show code actions"        })
+  vim.keymap.set('n',          '<M-d>', vim.diagnostic.open_float,  { buffer = bufnr, desc = "LSP: Show line diagnostics"    })
 end
 
 local on_java_attach = function(client, bufnr)
@@ -46,14 +42,14 @@ enable_lsp("jdtls", { on_attach = on_java_attach })
 
 enable_lsp("basedpyright", {
   on_attach = on_attach,
-  settings = {
+  settings  = {
     basedpyright = {
-      analysis = {
+      analysis   = {
         diagnosticSeverityOverrides = {
-          reportExplicitAny = "none",
+          reportExplicitAny         = "none",
           reportUnknownVariableType = "none",
           reportUnknownArgumentType = "none",
-          reportAny = "none",
+          reportAny                 = "none",
         },
       },
     },
@@ -62,22 +58,23 @@ enable_lsp("basedpyright", {
 
 enable_lsp("clangd", {
   init_options = {
-    fallbackFlags = { '-Wall', '-Wextra' } 
+    fallbackFlags = { '-Wall', '-Wextra' }
   },
   clang_user_options = ' -DCLANG_COMPLETE_ONLY',
-  on_attach = on_attach,
+  on_attach          = on_attach,
 })
 
-enable_lsp("hls", { on_attach = on_attach })
-enable_lsp("phpactor", { on_attach = on_attach })
-enable_lsp("intelephense", { on_attach = on_attach })
+enable_lsp("hls",                    { on_attach = on_attach })
+enable_lsp("phpactor",               { on_attach = on_attach })
 enable_lsp("kotlin_language_server", { on_attach = on_attach })
+enable_lsp("bashls",                 { on_attach = on_attach })
+enable_lsp("assembly",               { on_attach = on_attach })
 
 enable_lsp("racket_langserver", {
   cmd = { "racket", "--lib", "racket-langserver" },
   filetypes = { "racket", "scheme" },
   offset_encoding = "utf-8",
-  root_dir = function(fname)
+  root_dir = function(_)
     return vim.fn.getcwd()
   end,
   single_file_support = true,
@@ -85,11 +82,21 @@ enable_lsp("racket_langserver", {
 })
 
 enable_lsp("rust_analyzer", {
+  on_attach = on_attach,
   cmd = { "rust-analyzer" },
 })
 
-enable_lsp("bashls", {
+
+
+enable_lsp("lua_ls", {
   on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        disable = { "trailing-space" }, 
+      },
+    },
+  },
 })
 
 
@@ -109,7 +116,7 @@ require('lint').linters_by_ft = {
 
 do
   local guix_profile = os.getenv("HOME") .. "/.guix-profile"
-  local user_site = guix_profile .. "/share/guile/site/3.0"
+  local user_site   = guix_profile .. "/share/guile/site/3.0"
   local user_ccache = guix_profile .. "/lib/guile/3.0/site-ccache"
 
   enable_lsp("guile_lsp_server", {
